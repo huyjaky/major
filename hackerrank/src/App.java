a6aaa426c68e6171997e23b40627d3a4afcefada
huyjaky <huyphanuyg@gmail.com>
2022-10-20 08:05:43 +0700

    up

--- hackerrank/src/App.java
+++ hackerrank/src/App.java
@@ -1,5 +1,11 @@
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pattern regex = Pattern.compile("^[0-255]{1}.*[0-255]{1}.*[0-255]{1}.*[0-255]{1}$");
+        String s = "000.12.12.034" ;
+        if (regex.matcher(s).find()) {
+            System.out.println(s);
+        }
     }
 }
