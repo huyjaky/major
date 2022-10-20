436530873f56a488b4a1ada39caa643adb4b5a8f
huyjaky <huyphanuyg@gmail.com>
2022-10-20 16:09:00 +0700

    ban nhap ve regex

--- test/src/App.java
+++ test/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+        input.close();
+        Pattern regex = Pattern.compile(".*"+str+".*");
+        String[] array = {"phan duc huy", "pham gia bao"};
+        for (String i : array) {
+            if (regex.matcher(i).find()) {
+                System.out.println(i);
+                System.out.println(i.lastIndexOf(" "));
+            }
+        }
+
+
     }
 }
