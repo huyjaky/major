0424d6a34dba8281b9423c59f5d95dc1b765b4d0
huyjaky <huyphanuyg@gmail.com>
2022-10-21 20:57:15 +0700

    regex2

--- regexHackerRank2/src/App.java
+++ regexHackerRank2/src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //aPattern regex = Pattern.compile(null);
+        Scanner input = new Scanner(System.in);
+        String S = input.nextLine();
+        input.close();
+        S = S.replaceAll("[^a-zA-Z]", ".");
+        System.out.println(S);
     }
 }
