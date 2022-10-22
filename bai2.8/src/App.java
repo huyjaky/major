efe28c25a4cf13a78c07869cc028e0038ce19251
huyjaky <huyphanuyg@gmail.com>
2022-10-22 18:35:24 +0700

    up

---
+++ bai2.8/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        Integer day = input.nextInt();
+        input.close();
+        
+        Integer years = 0;
+        Integer months = 0;
+        Integer days = 0;
+        while (true) {
+            if (day > 365) {
+                years += 1;
+                day -= 365;
+            } else if (day < 365 && day >= 30) {
+                months += 1;
+                day -= 30;
+            } else if (day < 30) {
+                days = day;
+                break;
+            }
+        }
+        System.out.println(years); System.out.println(months); System.out.println(days);
+    }
+}
