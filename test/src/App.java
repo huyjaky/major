e300b110f850bb41f61a85ec4890f9938bb88f39
huyjaky <huyphanuyg@gmail.com>
2022-10-23 19:55:21 +0700

    sync

--- test/src/App.java
+++ test/src/App.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+
 public class App {
+    private Integer number1 = 1;
+    private Integer number = 1;
+
+    public static Integer loop (Integer number1, Integer number2) {
+        if (number2 > 100) {
+            return number1;
+        }
+        Integer temp = number2;
+        number2 += number1;
+        number1 = temp;
+        System.out.println(number1);
+        return loop(number1, number2);
+    }
+    
+    
     public static void main(String[] args) throws Exception {
         // Scanner input = new Scanner(System.in);
         // String str = input.nextLine();
@@ -14,9 +30,9 @@ public class App {
         //         System.out.println(i.lastIndexOf(" "));
         //     }
         // }
+        // System.out.println("script for present java history".toUpperCase());
+        loop(1, 1);
         
-        
-
         
     }
 }
