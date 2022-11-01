da1e3f8c6c9db2472e79a65e779396a7c58dd845
huyjaky <huyphanuyg@gmail.com>
2022-11-01 18:07:24 +0700

    sync

--- test2/src/App.java
+++ test2/src/App.java
@@ -1,11 +1,8 @@
-package test;
-
 import java.io.File;
 
-public class test {
-    public static void main(String[] args) {
+public class App {
+    public static void main(String[] args) throws Exception {
         File folder1 = new File("I:/newjava/major/test/src/test");
         System.out.println(folder1.exists());
-
     }
 }
